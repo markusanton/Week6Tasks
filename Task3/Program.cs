677d11e2bea1fc9fcac2d0409b1f083cff4c7608
markusanton <markusa070@gmail.com>
2020-10-07 15:37:35 +0300

    Add project files.

---
+++ Task3/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+
+namespace Task3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] randomArray = new int[10];
+            Random rnd = new Random();
+            for (int i = 0; i < randomArray.Length; ++i)
+                randomArray[i] = rnd.Next(1, 11);
+            int max = randomArray.Max();
+            int min = randomArray.Min();
+
+            Console.WriteLine($"Minimum: {min}");
+            Console.WriteLine($"Max: {max}");
+        }
+    }
+}
